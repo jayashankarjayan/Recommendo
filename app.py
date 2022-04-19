3b5d33d1306aed01a18ca962181569841d037718
Jayanand-Jayan <nandujayanand@gmail.com>
2022-04-19 21:19:34 +0530

    Login Page Backend

--- app.py
+++ app.py
@@ -6,70 +6,70 @@ app = Flask(__name__)
 ratings = pd.read_csv("ml-latest-small/ratings.csv")
 
 
-@app.route('/Home.html', methods=['POST', 'GET'])
+@app.route('/Home', methods=['POST', 'GET'])
 
 def home():
     return render_template('Home.html')
 
 
-@app.route('/Books.html', methods=['POST', 'GET']) 
+@app.route('/Books', methods=['POST', 'GET']) 
 
 def books():
     return render_template('Books.html')
 
-@app.route('/Movies.html', methods=['POST', 'GET'])
+@app.route('/Movies', methods=['POST', 'GET'])
 
 def movies():
     return render_template('Movies.html', ratings=ratings.to_dict())
 
-@app.route("/TV_Shows.html", methods=['POST', 'GET'])
+@app.route("/TV_Shows", methods=['POST', 'GET'])
 
 def tvshows():
     return render_template('TV_Shows.html')
 
-@app.route('/Music.html', methods=['POST', 'GET'])
+@app.route('/Music', methods=['POST', 'GET'])
 
 def music():
     return render_template('Music.html')
 
 
-@app.route("/About.html", methods=['POST', 'GET'])
+@app.route("/About", methods=['POST', 'GET'])
 
 def about():
     return render_template("About.html")
 
-@app.route("/Explore.html", methods=['POST', 'GET'])
+@app.route("/Explore", methods=['POST', 'GET'])
 
 def explore():
     return render_template("Explore.html")
 
-@app.route('/ROTD.html', methods=['POST', 'GET'])
+@app.route('/ROTD', methods=['POST', 'GET'])
 
 def rotd():
     return render_template("ROTD.html")
 
 
-@app.route('/Book_Solo.html', methods=['POST', 'GET'])
+@app.route('/Book_Solo', methods=['POST', 'GET'])
 
 def booksolo():
     return render_template("Book_Solo.html")
 
-@app.route('/Movie_Solo.html', methods=['POST', 'GET'])
+@app.route('/Movie_Solo', methods=['POST', 'GET'])
 
 def moviesolo():
     return render_template("Movie_Solo.html")
 
-@app.route('/Song_Solo.html', methods=['POST', 'GET'])
+@app.route('/Song_Solo', methods=['POST', 'GET'])
 
 def songsolo():
     return render_template("Song_Solo.html")
 
-@app.route('/TV_Show_Solo.html', methods=['POST', 'GET'])
+@app.route('/TV_Show_Solo', methods=['POST', 'GET'])
 
 def tvshowsolo():
     return render_template("TV_Show_Solo.html")
 
-@app.route("/login.html", methods=['POST', 'GET'])
+@app.route("/Login", methods=['POST', 'GET'])
 
 def login():
     error = None
