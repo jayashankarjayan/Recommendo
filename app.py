4122a2627931d909b47c153eb78d30c2d8f4169d
jayashankar.jayan <jayashankar.jayan@gmail.com>
2022-04-24 18:28:24 +0530

    corrected blueprints implementations, and performed code refactoring

--- app.py
+++ app.py
@@ -1,40 +1,24 @@
+import os
 from flask import Flask
-from flask_login import LoginManager, login_required
+from routes import GENERIC_BLUEPRINT
+from movies.routes import MOVIES_BLUEPRINT
+from music.routes import MUSIC_BLUEPRINT
+from shows.routes import SHOWS_BLUEPRINT
+from books.routes import BOOKS_BLUEPRINT
+from user.routes import USER_BLUEPRINT
+from authentication.routes import AUTH_BLUEPRINT
 
-from flask_sqlalchemy import SQLAlchemy
-from flask_bcrypt import Bcrypt 
-from sqlalchemy import orm 
+APP = Flask(__name__)
+APP.secret_key = os.urandom(32)
 
-app = Flask(__name__)
-app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-app.config["SQLALCHEMY_DATABASE_URI"] = "mysql+pymysql://root:@localhost/recommendo_login"
-app.secret_key = "My Secret Key"
-
-login_manager = LoginManager()
-login_manager.init_app(app)
-
-db = SQLAlchemy(app)
-
-bcrypt = Bcrypt(app)
-
-from routes import blueprint
-app.register_blueprint(blueprint)
-
-from models import User
-@login_manager.user_loader
-def load_user(user_id):
-    try:
-        return User.query.get(user_id)
-    except:
-        return None
-
-@login_required
-def loginreq():
-    pass
-
-
-        
+APP.register_blueprint(GENERIC_BLUEPRINT)
+APP.register_blueprint(MOVIES_BLUEPRINT)
+APP.register_blueprint(MUSIC_BLUEPRINT)
+APP.register_blueprint(SHOWS_BLUEPRINT)
+APP.register_blueprint(BOOKS_BLUEPRINT)
+APP.register_blueprint(USER_BLUEPRINT)
+APP.register_blueprint(AUTH_BLUEPRINT)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    APP.run(debug=True)
 
