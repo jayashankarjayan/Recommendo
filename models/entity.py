d57ea39ff67e1be14e11cd72e4e06f90b6c6e9e6
jayashankar.jayan <jayashankar.jayan@gmail.com>
2022-04-24 18:30:53 +0530

    corrected user entity

--- models/entity.py
+++ models/entity.py
@@ -1,27 +1,9 @@
-import os
-from sqlalchemy.orm import Session
-from sqlalchemy import create_engine
-from sqlalchemy import Column, String
-from sqlalchemy.orm import declarative_base
-from dotenv import load_dotenv
-
-load_dotenv()
-DB_NAME = os.environ.get("DB_NAME") 
-DB_USER = os.environ.get("DB_USER")
-DB_PASSWORD = os.environ.get("DB_PASSWORD")
-DB_HOST = os.environ.get("DB_HOST")
-CONN = f"mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}/{DB_NAME}"
-
-Base = declarative_base()
-engine = create_engine(CONN, future=True)
-session = Session(engine)
-
+from sqlalchemy import Column, String, Integer
+from utils.connection import Base
 
 class User(Base):
     __tablename__ = 'user'
 
-    username = Column(String(50), primary_key = True)
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    username = Column(String(50), nullable=False)
     password = Column(String(50), nullable=False)
-
-    def __repr__(self):
-        return '<User %r>' %self.username
