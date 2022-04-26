e8a8b17e4959538840924cdf58fff97051916b62
jayashankar.jayan <jayashankar.jayan@gmail.com>
2022-04-27 02:10:14 +0530

    added SPA with DRF to get similar books based on genre or summary similarity

---
+++ books/views.py
@@ -0,0 +1,75 @@
+import random
+
+import requests
+from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from django.shortcuts import render, redirect
+from rest_framework.renderers import TemplateHTMLRenderer
+
+from recommendo.settings import ES_BASE_URL, ES_INDEX, SEARCH_PAYLOAD, \
+                                DISTINCT_GENRES
+from books import model
+
+url = ES_BASE_URL + "/_search?pretty=true"
+
+
+class Genre(APIView):
+    renderer_classes = [TemplateHTMLRenderer]
+    template_name = 'skeleton.html'
+
+    def get(self, request, genre=None):
+        if genre is None:
+            genre = random.choice(Genre.get_unique_genres())
+
+        query_vector = model.encode([genre]).flatten().tolist()
+
+        SEARCH_PAYLOAD["query"]["script_score"]["script"]["source"] = SEARCH_PAYLOAD["query"]["script_score"]["script"]["source"].format("genre_enc")
+        SEARCH_PAYLOAD["query"]["script_score"]["script"]["params"]["query_vector"] = query_vector
+
+        response = requests.get(url, json=SEARCH_PAYLOAD).json()
+        records = []
+
+        data = response.get("hits", {}).get("hits", [])
+        genres = []
+        if data:
+            for record in data:
+                name = record["_source"]["name"]
+                genre = record["_source"]["genre"]
+                synopsis = record["_source"]["synopsis"]
+                records.append({"name": name, "genre": genre, "synopsis": synopsis})
+        else:
+            genres = Genre.get_unique_genres()
+
+        return Response({'books': records, "genres": genres})
+
+    @staticmethod
+    def get_unique_genres():
+        genres = []
+        response = requests.get(url, json=DISTINCT_GENRES).json()
+        for object in response.get("aggregations", {}).get("keys", {}).get("buckets", []):
+            genres.append(object["key"])
+
+        return genres
+
+class Synopsis(APIView):
+    renderer_classes = [TemplateHTMLRenderer]
+    template_name = 'skeleton.html'
+
+    def get(self, request, synopsis):
+        query_vector = model.encode([synopsis]).flatten().tolist()
+
+        SEARCH_PAYLOAD["query"]["script_score"]["script"]["source"] = SEARCH_PAYLOAD["query"]["script_score"]["script"]["source"].format("synopsis_enc")
+        SEARCH_PAYLOAD["query"]["script_score"]["script"]["params"]["query_vector"] = query_vector
+
+        url = ES_BASE_URL + "/_search?pretty=true"
+        response = requests.get(url, json=SEARCH_PAYLOAD).json()
+        data = response.get("hits", {}).get("hits", [])
+        records = []
+        for record in data:
+            name = record["_source"]["name"]
+            genre = record["_source"]["genre"]
+            synopsis = record["_source"]["synopsis"]
+            records.append({"name": name, "genre": genre, "synopsis": synopsis})
+
+        return Response({'books': records})
---
+++ recommendo/urls.py
@@ -0,0 +1,29 @@
+"""recommendo URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/4.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path, include
+from rest_framework import routers
+from books import views
+
+
+router = routers.DefaultRouter()
+
+
+urlpatterns = [
+    path('books/genre', views.Genre.as_view(), name="default_page"),
+    path('books/genre/<str:genre>', views.Genre.as_view(), name="genre_recommendation"),
+    path('books/synopsis/<str:synopsis>', views.Synopsis.as_view(), name="synopsis_recommendation")
+]
